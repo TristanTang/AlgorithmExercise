e14414d33026350c7c749b46d21e3e4b4342af46
WangTang <tangwang2008@gmail.com>
2019-10-17 06:27:16 +0800

    solved four problems

---
+++ src/DefangIpAddress.java
@@ -0,0 +1,33 @@
+/**
+ * @ClassName DefangIpAddress
+ * @Description 1108.IP 地址无效化。成绩： 1108, 0ms，用时击败 100%，内存击败 100%
+ * @Author Tristan
+ * @Date 2019/10/16 20:24
+ * @Version 1.0
+ **/
+public class DefangIpAddress {
+    /**
+     * @Description 将输入字符串中 "." 替换为 "[.]"
+     * @date 20:26 2019/10/16
+     * @param address IPV4 地址
+     * @return java.lang.String 替换后的字符串
+     * @author Tristan
+     */
+    public String defangIpAddr(String address) {
+        StringBuilder result = new StringBuilder();
+        for(char c : address.toCharArray()) {
+            if(c=='.') {
+                result.append("[.]");
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String address="255.100.50.0";
+        DefangIpAddress defangIpAddress = new DefangIpAddress();
+        System.out.println(defangIpAddress.defangIpAddr(address));
+    }
+}
